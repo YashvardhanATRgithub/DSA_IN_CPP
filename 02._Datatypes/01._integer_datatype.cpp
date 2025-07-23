6a5e17b6828ef88f8a413c8ebf9cfdb54206ba87
Yashvardhan <yashvardhansingh1009@gmail.com>
2025-07-23 09:42:29 +0530

    Add section 01 and section 02

---
+++ 02._Datatypes/01._integer_datatype.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int, long, long long
+
+    int x;
+    cin>>x;
+    cout<<x;
+}
---
+++ 02._Datatypes/03._getline.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    cout<<str;
+}
+
+// output: Apple is a fruit.
+// getline() prints all string present in a line
